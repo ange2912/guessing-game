97c790fbab21b66c5a887ab04d90bac70fd284ea
ange2912 <genachumakov@mail.ru>
2019-10-03 00:26:14 +0300

    feat: add solution guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+   
     guess() {
-
+        this.rangeValues=[];
+        for (var i=this.min; i<=this.max; i++){
+        this.rangeValues.push(i);
+    }     
+        return this.guessValue = Math.floor(this.rangeValues.length/2)+this.min;
     }
 
     lower() {
-
+        this.max = this.guessValue;
     }
 
     greater() {
-
+        this.min = this.guessValue;
     }
 }
 
